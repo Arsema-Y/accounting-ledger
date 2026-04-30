f7e3aabf1446ff3293fd8776772d1f349dc7b4de
mili Arsema <meleymm88@gmail.com>
2026-04-29 23:27:26 -0500

    Creates transaction class & functions

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -13,7 +13,7 @@ public class App {
    1. Plan out page ☑️
    2. Create Home Page Method ☑️
    3. Create following methods
-   4. Plan out back end page
+   4. Plan out back end page ☑️
    4. create backend functions
     */
 
@@ -24,19 +24,17 @@ public class App {
         runHome();
 
 
-
-
     }
 
 
     ///O: Welcome + sign in page
 
-    ///H: Home page - show option of what action user wants to take
-    private static void runHome(){
+    /// H: Home page - show option of what action user wants to take
+    private static void runHome() {
         boolean runHome = true;
 
 
-        while(runHome){
+        while (runHome) {
             System.out.println("Welcome to BitBook!\n");
             System.out.println("A trusted and reliable \n Accounting Ledger\n");
 
@@ -52,30 +50,30 @@ public class App {
             String email = input.nextLine();
 
 
-            while(!email.contains("@") || !email.contains(".")){
+            while (!email.contains("@") || !email.contains(".")) {
                 System.out.println("Invalid email.");
                 System.out.print("Please enter a valid email: ");
                 email = input.nextLine();
             }
 
-          //prompt name
+            //prompt name
             System.out.print("Enter your name: ");
             userName = input.nextLine();
 
-          //Home: screens choice
-            System.out.println("Hello " + userName +",");
+            //Home: screens choice
+            System.out.println("Hello " + userName + ",");
             System.out.println("With BitBook, you can   D - Add a Deposit\n" +
                     "                        P - Make a Payment \n" +
                     "                        L - View Ledger \n" +
                     "                        X - Exit BitBook");
             char level2Screen = input.next().toUpperCase().charAt(0);
-            switch(level2Screen){
+            switch (level2Screen) {
                 case 'D' -> runDepositScreen();
                 case 'P' -> runPaymentScreen();
                 case 'L' -> runLedgerScreen();
                 case 'X' -> {
                     runHome = false;
-                    if(!runHome){
+                    if (!runHome) {
                         System.out.println("Thank You for using BitBook.");
                     }
                 }
@@ -86,24 +84,22 @@ public class App {
     }
 
 
-
-
-    ///D: Add Deposit - loop: prompt user for deposit information
+    /// D: Add Deposit - loop: prompt user for deposit information
     /* + save it to the transactions file
      * + H: Home - run Home method
      * + X: Exit
      */
-    private static void runDepositScreen(){
+    private static void runDepositScreen() {
         boolean deposit = true;
-        while(deposit) {
+        while (deposit) {
             System.out.println("+ - Add another deposit");
             char addMore = input.next().toUpperCase().charAt(0);
-             if(addMore == +){
-                 boolean add = true;
-                 while(add){
-                     //add deposit loop
-                 }
-             }
+            if (addMore == +) {
+                boolean add = true;
+                while (add) {
+                    //add deposit loop
+                }
+            }
 
             System.out.println("H - Home");
             System.out.println("X - Done");
@@ -112,128 +108,123 @@ public class App {
                 case 'H' -> deposit = false;
                 case 'X' -> {
                     deposit = false;
-                    if(!deposit){
+                    if (!deposit) {
                         System.out.println("Thank You for using BitBook.");
                     }
+                }
             }
         }
     }
 
 
-
-
-
-///P: Make Payments (Debit) - loop: prompt user for debit information
+    /// P: Make Payments (Debit) - loop: prompt user for debit information
 //  + save it to the transactions file
-private static void runPaymentScreen(){
-    System.out.println("test");
+    private static void runPaymentScreen() {
+        System.out.println("test");
 
 
-}
+    }
 
 
+    /// L: Ledger - shows options of display method
+    private static void runLedgerScreen() {
+        System.out.println("test");
 
 
-///L: Ledger - shows options of display method
-private static void runLedgerScreen(){
-    System.out.println("test");
+    }
 
 
-}
+    /// A: All - display all entries (newest - oldest)
+    private static void runAllEnteriesScreen() {
+        System.out.println("test");
 
 
-///A: All - display all entries (newest - oldest)
-private static void runAllEnteriesScreen(){
-    System.out.println("test");
+    }
 
 
-}
-
-
-///D: Deposits - display only deposits into the account (positive values)
+    /// D: Deposits - display only deposits into the account (positive values)
 //H: Home - back to Home method
 // + X: Exit
-private static void runDepoitsScreen(){
-    System.out.println("test");
+    private static void runDepoitsScreen() {
+        System.out.println("test");
 
 
-}
+    }
 
 
-///P: Payments - display the payments only (negative values)
+    /// P: Payments - display the payments only (negative values)
 //H: Home - back to Home method
 // + X: Exit
-private static void runPaymentsScreen(){
-    System.out.println("test");
+    private static void runPaymentsScreen() {
+        System.out.println("test");
 
 
-}
+    }
 
 
-///R: Reports - shows option for display method
-private static void runReportsScreen(){
-    System.out.println("test");
+    /// R: Reports - shows option for display method
+    private static void runReportsScreen() {
+        System.out.println("test");
 
 
-}
+    }
 
 
-///1: Month to date - start of current month till present day
-private static void runPresentMonthScreen(){
-    System.out.println("test");
+    /// 1: Month to date - start of current month till present day
+    /* 0: Ledger - back to ledger method
+     * H: Home
+     */
+    private static void runPresentMonthScreen() {
+        System.out.println("test");
 
 
-}
+    }
 
 
-///2: Previous Month - month before present month (MM-1)
-private static void runLastMonthScreen(){
-    System.out.println("test");
+    /// 2: Previous Month - month before present month (MM-1)
+    /* 0: Ledger - back to ledger method
+     * H: Home
+     */
+    private static void runLastMonthScreen() {
+        System.out.println("test");
 
 
-}
+    }
 
 
-///3: Year to Date - new year till present day
-private static void runPresentYearScreen(){
-    System.out.println("test");
+    /// 3: Year to Date - new year till present day
+    /* 0: Ledger - back to ledger method
+     * H: Home
+     */
+    private static void runPresentYearScreen() {
+        System.out.println("test");
 
 
-}
+    }
 
 
-///4: Search by Vendor - prompts user for the vendor name
-/*   + runs comparison with given name
- *   + displays matching entries
- */
-private static void runSearchScreen(){
-    System.out.println("test");
+    /// 4: Search by Vendor - prompts user for the vendor name
+    /*   + runs comparison with given name
+     *   + displays matching entries
+     * 0: Ledger - back to ledger method
+     * H: Home
+     */
+    private static void runSearchScreen() {
+        System.out.println("test");
 
 
-}
-}
-
-///0: Ledger - back to ledger method
+    }
 
 
-///H: Home
-
-
-
-
-///H: Home - back to Home method
+    ///H: Home - back to Home method
 //+ X: Exit
 
 
-
-
-
-
 ///x: Exit
-/* closing message
- * H: Home - back to home
- */
+    /* closing message
+     * H: Home - back to home
+     */
 
 
-
+}
 
